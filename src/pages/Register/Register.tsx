1167ee856c92345dc6c6345fbe740eb9a17590ef
Mateen Mirani <mateenmirani125@gmail.com>
2021-11-30 18:38:35 +0500

    Create pages

---
+++ src/pages/Register/Register.tsx
@@ -0,0 +1,30 @@
+import {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+  } from "@ionic/react";
+  import "./Register.css";
+  
+  const Register: React.FC = () => {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Register</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+          <IonHeader collapse="condense">
+            <IonToolbar>
+              <IonTitle size="large">Register</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+        </IonContent>
+      </IonPage>
+    );
+  };
+  
+  export default Register;
+  
